6f5c43e80a94881dd784561c246075f7180870dc
Scott Jordan <scottjordanswimming@gmail.com>
2019-09-01 17:22:19 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,5 +3,5 @@ def meal_choice(protein = "meat", veg1, veg2)
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
 end
 
-meal_choice("meat", "broccoli", "macaroni")
+meal_choice("tofu", "broccoli", "macaroni")
 meal_choice("broccoli", "macaroni")
