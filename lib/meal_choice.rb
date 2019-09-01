64d23feed401e00996cd0eacf7f9dfbfd04a96bc
Scott Jordan <scottjordanswimming@gmail.com>
2019-09-01 17:32:33 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,6 +1,6 @@
-def meal_choice(protein = "meat", veg1, veg2)
+def meal_choice (veg1, veg2, protein = "meat")
   puts "What a nutritious meal!"
   puts "A plate of #{protein} with #{veg1} and #{veg2}."
 end
 
-meal_choice("tofu", "broccoli", "macaroni")
+meal_choice("broccoli", "macaroni", "tofu")
